ed2c2fa64d3b14dd7b781b9e02193eae7760c7ca
Nadim <knrs986@astrazeneca.net>
2024-12-18 12:39:30 +0530

    modify ingress script

--- AZ/ingress-automation/python_script.py
+++ AZ/ingress-automation/python_script.py
@@ -3,7 +3,6 @@ import yaml
 import argparse
 import os
 
-
 def get_file_name(cluster,namespace):
     if cluster == 'dev':
         sort_namespace = "-".join(namespace.split("-")[-2:])
@@ -23,10 +22,33 @@ def get_file_name(cluster,namespace):
     print(f"file_name in namespace {namespace} is {file_name}")
     return file_name
 
-def create_ingress_manifest(ingress_data):
+def check_new_line(cluster,filename):
+    file = f"../states/{cluster}/{filename}"
+    with open(file, 'rb+') as f:
+        f.seek(-1, 2)
+        last_byte = f.read(1)
+        if last_byte == b'-':
+            f.write(b'\n')
+        elif last_byte == b'\n':
+            f.seek(-2,2)
+            second_line=f.read(1)
+            if second_line == b'-':
+                pass
+            else:
+                f.write(b'\n---\n')
+        else:
+            f.write(b'\n---\n')
+
+def create_ingress_manifest(*args):
     env = Environment(loader=FileSystemLoader('../templates'))
     template = env.get_template('ingress_template.yaml.j2')
-    rendered_yaml = template.render(values=ingress_data)
+    rendered_yaml = template.render(
+        NAME = name,
+        NAMESPACE = namespace,
+        HOST = host,
+        SERVICE = service,
+        PORT = port
+    )
     return rendered_yaml
 
 def merge_ingress(cluster,file_name,ingress_manifest):
@@ -35,7 +57,6 @@ def merge_ingress(cluster,file_name,ingress_manifest):
         f.write(ingress_manifest)
         print(f"YAML file '{filename}' created successfully.")
         
-
 parser = argparse.ArgumentParser(description="required values for Ingress manifest file")
 parser.add_argument("-c", "--clusterName", help="cluster_name")
 parser.add_argument("-a", "--namespace", help="namespace")
@@ -52,32 +73,10 @@ service = args["service"]
 port = args["port"]
 host = args["host"]
 
-
-ingress_data = [{
-    'metadata': {
-        'name': name,
-        'namespace': namespace
-    },
-    'spec': {
-        'rules': [{
-            'host': host,
-            'http': {
-                'paths': [{
-                    'backend': {
-                        'service': {
-                            'name': service,
-                            'port': {
-                                'number': port
-                            }
-                        }
-                    }
-                }]
-            }
-        }]
-    }  
-}]
-
-
 filename = get_file_name(cluster, namespace)
-ingress_manifest = create_ingress_manifest(ingress_data)
+check_new_line(cluster,filename)
+ingress_manifest = create_ingress_manifest(name,namespace,host,service,port)
 merge_ingress(cluster,filename,ingress_manifest)
+check_new_line(cluster,filename)
+
+# python generate_ingress_manifest.py -c dev -a brown-dev-005 -n ingress-test -s test-service -p 80 -h test.paas-brown.as.com
